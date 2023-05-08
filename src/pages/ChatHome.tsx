e0e338a8f823f7a049b92b2edaf9e862103de209
dinasoa <hei.dinasoa@gmail.com>
2023-05-08 19:28:57 +0300

    feat: add login page and save data in the local storage

---
+++ src/pages/ChatHome.tsx
@@ -0,0 +1,7 @@
+export default function ChatHome () {
+    return(
+        <>
+            <h1> Ici sera le home page de notre chat</h1>
+        </>
+    )
+}
---
+++ src/pages/Form.tsx
@@ -0,0 +1,90 @@
+import { useState } from 'react';
+import styles from '@/styles/Form.css';
+import Link from "next/link";
+
+export default function Form() {
+    const [email, setEmail] = useState('');
+    const [name, setName] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleFormSubmit = (event) => {
+        event.preventDefault();
+
+        const userInfo = {
+            email: email,
+            name: name,
+            password: password
+        };
+
+        localStorage.setItem('userInfo', JSON.stringify(userInfo));
+        alert('Formulaire soumis avec succès !');
+    };
+
+    const handleInputChange = (event) => {
+        const eventName = event.target.name;
+        const value = event.target.value;
+
+        if (eventName === 'email') {
+            setEmail(value);
+        } else if (eventName === 'name') {
+            setName(value);
+        } else if (eventName === 'password') {
+            setPassword(value);
+        }
+    };
+
+    return (
+        <>
+            <div className="card">
+                <form className="form" onSubmit={handleFormSubmit}>
+                    <div className="email-section">
+                        <label htmlFor="email" className="label">
+                            Email
+                        </label>
+                        <input
+                            className="input"
+                            id="email"
+                            name="email"
+                            type="email"
+                            value={email}
+                            onChange={handleInputChange}
+                        />
+                    </div>
+                    <div>
+                        <label htmlFor="name" className="label">
+                            Name
+                        </label>
+                        <input
+                            className="input"
+                            id="name"
+                            name="name"
+                            type="text"
+                            value={name}
+                            onChange={handleInputChange}
+                        />
+                    </div>
+                    <div>
+                        <label htmlFor="password" className="label">
+                            Password
+                        </label>
+                        <input
+                            className="input"
+                            id="password"
+                            name="password"
+                            type="password"
+                            value={password}
+                            onChange={handleInputChange}
+                        />
+                    </div>
+
+                    <Link href="/ChatHome">
+                        <button className="button" type="submit" onClick={handleFormSubmit}>
+                        Se connecter
+                        </button>
+                    </Link>
+
+                </form>
+            </div>
+        </>
+    );
+}
